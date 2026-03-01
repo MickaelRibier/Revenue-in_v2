f30a466d3d3ff2a352ddc9f25d86f7ab0617ef3f
MickaelRibier <trips380@yahoo.fr>
2026-03-01 19:00:12 +0100

    auth.js update for prod

--- app/auth.js
+++ app/auth.js
@@ -25,7 +25,7 @@ if (process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET) {
             {
                 clientID: process.env.GOOGLE_CLIENT_ID,
                 clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-                callbackURL: '/auth/google/callback',
+                callbackURL: (process.env.BASE_URL || 'http://localhost:3000') + '/auth/google/callback',
             },
             async (accessToken, refreshToken, profile, done) => {
                 try {
